41489233546391e1ea586f1be63fd29f3522fd37
tiger <thisisstreamliner@gmail.com>
2023-03-30 20:57:06 +0300

    add filter to robots

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
-import CardList from './cardlist';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots = {robots}/>
+    <App />
   </React.StrictMode>
 );
 

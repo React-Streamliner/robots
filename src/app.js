2f1bde054e95834f63ec6309af473cab2b338beb
tiger <thisisstreamliner@gmail.com>
2023-04-01 19:49:10 +0300

    Retrieve robots from network request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CardList from "./cardlist";
-import { robots } from "./robots";
 import SearchBox from "./searchbox";
 import './app.css';
 
@@ -9,9 +8,16 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount() {
+        console.log('check')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots : users }))
     }
 
     onSearchChange = (event) => {
@@ -24,13 +30,17 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 

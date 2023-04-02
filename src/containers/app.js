c6dd931a67964901cd32ac25b16b67975d695f9d
tiger <thisisstreamliner@gmail.com>
2023-04-02 20:05:31 +0300

    refactor code stucture

---
+++ src/containers/app.js
@@ -0,0 +1,47 @@
+import React from "react";
+import CardList from "../components/cardlist";
+import SearchBox from "../components/searchbox";
+import './app.css';
+import Scroll from "../components/scroll";
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: '',
+        }
+    }
+
+    componentDidMount() {
+        console.log('check')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots : users }))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchfield: event.target.value
+        })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        return !robots.length
+            ? <h1>Loading</h1>
+            : <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+    }
+}
+
+export default App;

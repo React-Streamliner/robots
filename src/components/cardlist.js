07c7ab0c4fe3fd5c1e03c0dea91fd1de633f5fd7
tiger <thisisstreamliner@gmail.com>
2023-04-06 16:34:50 +0300

    Remove intented error from cardlsit

--- src/components/cardlist.js
+++ src/components/cardlist.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./card";
 
 const CardList = ({robots}) => {
-    if (true) {
-        throw new Error('Nooo');
-    }
     return (
         <div>
             {
